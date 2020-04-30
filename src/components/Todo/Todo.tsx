4540ed78d31302278806aa310b3094a79a555a93
jawakarD <jawakarsmith@gmail.com>
2020-04-30 18:00:26 +0530

    Add net-info

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-<<<<<<< HEAD
 import {
   ScrollView,
   ActivityIndicator,
@@ -7,20 +6,13 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-=======
-import {ScrollView, ActivityIndicator, View} from 'react-native';
->>>>>>> Use loding indicator
 import styles from './style';
 import {useDispatch, useSelector} from 'react-redux';
 import {TodosState, TodoState} from 'reducers/todo';
 import {getTodos, updateTodo} from 'actions/todo';
 import TodoRow from './TodoRow';
-<<<<<<< HEAD
 import {TodoProps} from './types';
 import {LOADING} from 'constants/uiStates';
-=======
-import {INIT, LOADING} from 'constants/uiStates';
->>>>>>> Use loding indicator
 
 const Todo = ({navigation}: TodoProps) => {
   const disptach = useDispatch();
--- src/components/Todo/TodoNavigator.tsx
+++ src/components/Todo/TodoNavigator.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Todo from './Todo';
-import {Image} from 'react-native';
+import {Image, View, Text} from 'react-native';
+import {useNetInfo} from '@react-native-community/netinfo';
+import styles from './style';
 
 export type TodoStackParamsList = {
   Todo: undefined;
@@ -9,23 +11,31 @@ export type TodoStackParamsList = {
 
 const Stack = createStackNavigator<TodoStackParamsList>();
 
-const LogoTitle = () => (
-  <Image
-    source={{
-      uri:
-        'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/090/thumb/EGH_ReactNative_ToDo-Final.png',
-      width: 40,
-      height: 40,
-    }}
-  />
-);
+const LogoTitle = () => {
+  const {isInternetReachable} = useNetInfo();
+  return (
+    <View style={styles.logoTitle}>
+      <Image
+        source={{
+          uri:
+            'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/090/thumb/EGH_ReactNative_ToDo-Final.png',
+          width: 40,
+          height: 40,
+        }}
+      />
+      {!isInternetReachable && (
+        <Text style={styles.notConnected}>Not Connected</Text>
+      )}
+    </View>
+  );
+};
 
 const TodoNavigator = () => (
   <Stack.Navigator
     initialRouteName="Todo"
     screenOptions={{
       headerStyle: {
-        backgroundColor: '#444',
+        backgroundColor: '#121212',
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
